8c3b19ab8add3757e3154db2ccb1b5c0fb24304b
Bileonaire <kiokoleon@gmail.com>
2018-11-11 22:06:47 +0300

    Handling error events and console-loging them to be read well

--- getRequest.js
+++ getRequest.js
@@ -3,29 +3,58 @@
 
 //Require https module
 const https = require('https');
-const username = "chalkers";
+
+//Require https module for status codes
+const http = require('http');
 
 //Function to print message to console
 function printMessage(username, badgeCount, points) {
-    const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`;
-    console.log(message);
+  const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`;
+  console.log(message);
 }
 
-// Connect to the API URL (https://teamtreehouse.com/username.json)
-const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+function getProfile(username) {
+  try {
+    // Connect to the API URL (https://teamtreehouse.com/username.json)
+    const request = https.get(
+      `https://teamtreehouse.com/${username}.json`,
+      response => {
+        if (response.statusCode === 200) {
+          let body = '';
+          // Read the data
+          response.on('data', data => {
+            body += data.toString();
+          });
 
-    let body = "";
-    // Read the data
-    response.on('data', data => {
-        body += data.toString();
-    });
+          // console.log(response);
+          response.on('end', () => {
+            // Parse the data
+            const profile = JSON.parse(body);
+            // console.dir(profile);
 
-    // console.log(response);
-    response.on('end', () => {
-        // Parse the data
-        const profile = JSON.parse(body);
-        console.dir(profile);
-        printMessage(username, profile.badges.length);
-    })
-    // Print the data
-});
+            // Print the data
+            printMessage(
+              username,
+              profile.badges.length,
+              profile.points.JavaScript
+            );
+          });
+        } else {
+          const message = `there was an error getting the profile for ${username} (${
+            http.STATUS_CODES[response.statusCode]
+          })`;
+          console.error(message);
+        }
+      }
+    );
+    request.on('error', error =>
+      console.error(`Problem with request: ${error.message}`)
+    );
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+// Getting arguments from the command line by running the file and putting argements after the command
+const users = process.argv.slice(2);
+users.forEach(getProfile);
